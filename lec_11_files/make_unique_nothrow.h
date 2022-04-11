b788bcedaaec326b79df7f5f3a64983172205c6f
Yifei Xu <yifeix@Yifeis-MacBook-Pro.local>
2022-04-11 17:41:51 -0500

    forget universal reference for perfect forwarding

--- lec_11_files/make_unique_nothrow.h
+++ lec_11_files/make_unique_nothrow.h
@@ -9,7 +9,7 @@ namespace hw11_1
 {
 
 template<typename T, typename ...  Args>
-std::unique_ptr<T> make_unique(Args... args) noexcept
+std::unique_ptr<T> make_unique(Args&& ... args) noexcept
 {
     return std::unique_ptr<T>(new(std::nothrow) T(std::forward<Args>(args)...));
 }
